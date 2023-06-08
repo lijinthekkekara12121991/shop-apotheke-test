d8b61349df0ace1f761862a360dc78e2d18a9460
lijinthekkekara12121991 <lijinj@gmail.com>
2023-06-08 18:21:18 +0200

    Add the tests in cypress default format

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    env: {
+      APP_URL: 'https://www.shop-apotheke.com/',
+      API_URL: 'https://api.sa-tech.de/static',
+      USERNAME: 'shop-user@redteclab.com',
+      PASSWORD: 'aA1!bB2@cC3#dD4$'
+    }
+  },
+};
